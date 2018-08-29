f93949b0d6c4eb83823e68243c8343e60a5a0bd2
Gabriel Passos <gabriel.passos@ilegra.com>
2018-08-29 14:01:37 -0300

    :construction: Trabalhando nas rotas do play

--- play-scala-ui-poc/app/controllers/MessagesController.scala
+++ play-scala-ui-poc/app/controllers/MessagesController.scala
@@ -6,7 +6,17 @@ import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Reque
 @Singleton
 class MessagesController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
+  case class UserData(name: String, age: Int)
+
   def getMessage() = Action{ implicit request: Request[AnyContent] =>
-    Ok(views.html.message())
+    Ok(views.html.message(""))
+  }
+
+  def getMessagePathParam(message: String) = Action{ implicit request: Request[AnyContent] =>
+    Ok(views.html.message(message))
+  }
+
+  def sendUser(username: String, userage: Int) = Action {implicit request: Request[AnyContent] =>
+    Ok(views.html.user(username, userage))
   }
 }
