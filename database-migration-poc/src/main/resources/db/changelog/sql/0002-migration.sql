ddb6da0d2b1b9973a342f56337d19bf924bf1dda
gabrielSpassos <gabrielsantos45725@gmail.com>
2025-10-24 09:11:52 -0300

    feat: update liquibase

--- database-migration-poc/src/main/resources/db/changelog/sql/0002-migration.sql
+++ database-migration-poc/src/main/resources/db/changelog/sql/0002-migration.sql
@@ -3,5 +3,8 @@
 -- changeset gpassos:0002-update-tables
 alter table "users" add column user_id uuid null;
 alter table "users" add column cpf text null;
+alter table "users" alter column external_id1 drop not null;
+alter table "users" alter column external_id2 drop not null;
+
 alter table reports add column user_id uuid null;
 -- rollback drop table users;
